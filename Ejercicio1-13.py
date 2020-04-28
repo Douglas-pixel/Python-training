491ce7dff5f8cdb537c1a44b705bb415f59c8c89
Douglas-pixel <douglasnavarrete10@gmail.com>
2020-04-27 23:11:07 -0600

    dona

--- Ejercicio1-13.py
+++ Ejercicio1-13.py
@@ -5,3 +5,4 @@ interest = int(input("a que tasa de interés: "))/100
 for i in range(0, years):
     savings = (savings + savings*interest)
     print("en el año ",(i+1)," usted tendrá en su cuenta de ahorro: ","{0:.2f}".format(savings)," dólares")
+print(2+2)
